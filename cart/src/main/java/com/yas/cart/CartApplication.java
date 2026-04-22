75cda22280416a3e64da5686509597e3b6969444
xyberr <sikea0801@gmail.com>
2026-04-22 16:30:57 +0700

    feat: add an extra line to test ci

--- cart/src/main/java/com/yas/cart/CartApplication.java
+++ cart/src/main/java/com/yas/cart/CartApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties({ServiceUrlConfig.class, CorsConfig.class})
 public class CartApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(CartApplication.class, args);
     }
